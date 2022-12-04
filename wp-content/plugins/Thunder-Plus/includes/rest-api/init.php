43e58ebd5765431e8f493fd4e94d0d58bda88815
mamirjamali <aj3848110@gmail.com>
2022-12-04 15:18:50 +0330

    Fix register_rest_route Method

--- wp-content/plugins/Thunder-Plus/includes/rest-api/init.php
+++ wp-content/plugins/Thunder-Plus/includes/rest-api/init.php
@@ -3,13 +3,13 @@
 function thp_rest_api_init(){
     //example.com/wp-jason/thp/v1/signup
     register_rest_route( 'thp/v1', '/signup', [
-        'methods' => 'POST',
+        'methods' => WP_REST_Server::CREATABLE,
         'callback' => 'thp_rest_api_signup_handler',
         'permission_callback' => '__return_true'
     ]);
 
     register_rest_route( 'thp/v1', '/signin', [
-        'methods' => 'POST',
+        'methods' => WP_REST_Server::EDITABLE,
         'callback' => 'thp_rest_api_signin_handler',
         'permission_callback' => '__return_true'
     ]);
