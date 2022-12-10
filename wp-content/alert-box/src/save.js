29c7585603103af034ab3b6c454c53fd2919f6b9
mamirjamali <aj3848110@gmail.com>
2022-12-10 18:02:32 +0330

    Add new Alert Box plugin

---
+++ wp-content/alert-box/src/save.js
@@ -0,0 +1,25 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { Icon } from '@wordpress/components';
+
+export default function save({ attributes }) {
+	const { content, icon, bgColor, textColor } = attributes
+	const blockProps = useBlockProps.save({
+		style: {
+			'border-color': bgColor,
+			'background-color': bgColor,
+			color: textColor
+		}
+	})
+
+	return (
+		<div { ...blockProps }>
+			<Icon 
+				icon={icon}
+			/>
+			<div class="alert-box-content">
+				<RichText.Content tagName="p" value={content} />
+			</div>
+		</div>
+	)
+}
